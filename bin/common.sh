841f01564ee172570cdb14abaaad980206865943
steve <stevehermes@gmail.com>
2013-12-22 07:28:50 -0600

    select localbuild of play framework 1.2.7

--- bin/common.sh
+++ bin/common.sh
@@ -24,7 +24,8 @@ check_compile_status()
 install_play()
 {
   VER_TO_INSTALL=$1
-  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  #######steve  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  PLAY_URL="http://powerhive.webfactional.com/home/powerhive/PLAY/play-1.2.x-localbuild.tar.gz"
   PLAY_TAR_FILE="play-heroku.tar.gz"
   echo "-----> Installing Play! $VER_TO_INSTALL....."
   curl --silent --max-time 150 --location $PLAY_URL -o $PLAY_TAR_FILE
